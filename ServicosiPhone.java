71c3caf030ca161cecb562229daa9ccda3b565ce
Pedro Madruga <pedromadruga6@gmail.com>
2025-01-28 21:31:22 -0300

    Update ServicosiPhone.java

--- ServicosiPhone.java
+++ ServicosiPhone.java
@@ -1,20 +1,7 @@
 package edu.PilaresPOO_IPC.DesafioiPhone.program;
 
-import edu.PilaresPOO_IPC.DesafioiPhone.aparelhotelefonico.AparelhoTelefonico;
-import edu.PilaresPOO_IPC.DesafioiPhone.navegadorinternet.NavegadorInternet;
 import edu.PilaresPOO_IPC.DesafioiPhone.reprodutormusical.ReprodutorMusical;
 
-public class ServicosiPhone extends NavegadorInternet{
-
-    NavegadorInternet navegadorInternet;
-    AparelhoTelefonico aparelhoTelefonico;
-    ReprodutorMusical reprodutorMusical;
-
-    public ServicosiPhone() {
-        this.navegadorInternet = new NavegadorInternet();
-        this.aparelhoTelefonico = new AparelhoTelefonico();
-        this.reprodutorMusical = new ReprodutorMusical();
-    }
-
+public class ServicosiPhone extends ReprodutorMusical{
 
 }
